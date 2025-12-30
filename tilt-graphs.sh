a970fa5164b17c1c0f65754d900c19feec75a042
Jeff Davis <jeff.davis@gmail.com>
2025-12-30 09:25:36 -0800

    more messages

--- tilt-graphs.sh
+++ tilt-graphs.sh
@@ -7,9 +7,13 @@ cd $DATA/scripts || (echo "FATAL: cd to $DATA/scripts failed"; exit 1)
 
 echo "******************** generate tilt graphs as of `date`"
 
+echo "*** Daily.R `date`"
 R --slave -s < daily.R
+
+echo "*** Showme.R `date`"
 R --slave -s < showme.R
 
+echo "*** rclone `date`"
 rclone sync $DATA/graphs/ s3:j3ff.com/graphs/
 
 echo "******************** graphs done as of `date`"
